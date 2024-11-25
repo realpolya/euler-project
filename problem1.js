9a5bbc4fb24274332195ea6cfc98a9162b285757
realpolya <ps2780@columbia.edu>
2024-11-24 20:15:13 -0500

    solve problem #1

--- problem1.js
+++ problem1.js
@@ -5,3 +5,23 @@ If we list all the natural numbers below $10$ that are multiples of $3$ or $5$, 
 Find the sum of all the multiples of $3$ or $5$ below $1000$.
 */
 
+const multiples53 = (limit) => {
+
+      const multiples = []
+
+      for (let i = 1; i < limit; i++) {
+            if (i % 3 === 0 || i % 5 === 0) {
+                  multiples.push(i)
+            }
+      }
+
+      return sum = multiples.reduce((arg, i) => {
+
+            return arg += i;
+
+      }, 0)
+      
+}
+
+console.log(multiples53(10))
+console.log(multiples53(1000))
