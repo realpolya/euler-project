68872c26cfe37ab44e20024c7daf4317b86810a4
realpolya <ps2780@columbia.edu>
2024-11-28 23:29:46 -0500

    in the process of solving 4

--- problem4.js
+++ problem4.js
@@ -9,13 +9,71 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 const largestPalindrome = digits => {
 
       // start from the largest numbers with the requested number of digits
+      let largestMultiplierArr = []
+      let smallestMultiplierArr = [1]
 
-      // multiply
+      for (let i = 0; i < digits; i++) {
+            largestMultiplierArr.push(9)
+            if (i < digits - 1) {
+                  smallestMultiplierArr.push(0)
+            }
+      }
 
-      // convert number into a string
+      let largestMultiplier = +largestMultiplierArr.join('')
+      let smallestMultiplier = +smallestMultiplierArr.join('')
 
-      // and see if exterior digist match
+      // console.log("largest multiplier is ", largestMultiplier)
+      // console.log("smallest multiplier is ", smallestMultiplier)
 
-            // if exterior digits match continue checking the digits
+      let product;
+
+      // loop within a loop
+      outerLoop: for (let i = largestMultiplier; i >= smallestMultiplier; i--) {
+
+            innerLoop: for (let j = largestMultiplier; j >= smallestMultiplier; j--) {
+
+                  console.log("i is ", i, "j is ", j)
+
+                  // multiply
+                  product = +j * +i
+            
+                  // convert number into a string
+                  let string = product.toString().split('')
+                  console.log('string is ', string)
+
+                  let y = string.length - 1
+                  let half = Math.floor(string.length / 2)
+                  let palindrome = true;
+
+                  // and see if exterior digits match
+                  miniLoop: for (let z = 0; z < half; z++) {
+
+                        console.log("y is ", y, "z is ", z)
+                        console.log("string at z is ", string[z], "string at y is ", string[y])
+
+                        // if exterior digits match continue checking the digits
+                        if (string[z] != string[y]) {
+                              console.log("not a palindrome")
+                              palindrome = false
+                              break miniLoop
+                        }
+                        y--
+
+                  }
+
+                  if (palindrome) {
+                        console.log("breaking outer loop, found the largest palindrome")
+                        break outerLoop
+                  }
+
+
+            }
+      }
+
+      console.log("palindrome is", product)
+      return product
 
 }
+
+// largestPalindrome(2)
+largestPalindrome(3)
