61f676ca7bbc15515446e999d08c06b909967833
realpolya <ps2780@columbia.edu>
2024-11-30 17:37:11 -0500

    work on problem 5

--- problem5.js
+++ problem5.js
@@ -2,8 +2,26 @@
 Smallest Multiple
 
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-What is the smallest positive number that is evenly divisible divisible with no remainder
+What is the smallest positive number that is evenly divisible (divisible with no remainder)
 by all of the numbers from 1 to 20?
 
 */
 
+const smallestMultiple = (range) => {
+
+      if (!Array.isArray(range) || range.length != 2) return "Range must be an array of two integers"
+
+      // start with multiplying the two largest numbers in range
+
+      // check if the number is even
+
+      // from there go through even numbers
+
+      // do range.ForEach or range.some to see if the condition is satisfied
+
+            // break the loop when the condition is not satisfied (Array.some breaks the loop automatically)
+
+      
+
+
+}
