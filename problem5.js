310d63235847486228e2cef7dba12ea031186d6d
realpolya <ps2780@columbia.edu>
2024-11-30 19:01:28 -0500

    solve problem 5

--- problem5.js
+++ problem5.js
@@ -11,17 +11,41 @@ const smallestMultiple = (range) => {
 
       if (!Array.isArray(range) || range.length != 2) return "Range must be an array of two integers"
 
+      let multiple;
+      const rangeArr = []
+      for (let i = +range[0]; i <= +range[1]; i++) {
+            rangeArr.push(i)
+      }
+
       // start with multiplying the two largest numbers in range
+      let candidate = rangeArr[rangeArr.length - 1] * rangeArr[rangeArr.length - 2]
 
       // check if the number is even
+      if (candidate % 2 !== 0) candidate++;
 
       // from there go through even numbers
+      while (!multiple) {
 
-      // do range.ForEach or range.some to see if the condition is satisfied
+            // do range.ForEach or range.some to see if the condition is satisfied
+            let notAnswer = rangeArr.some(divisor => {
+                  return candidate % divisor !== 0
+            })
 
-            // break the loop when the condition is not satisfied (Array.some breaks the loop automatically)
+            // if some is false
+            if (!notAnswer) {
 
-      
+                  // assign multiple
+                  multiple = candidate
 
+            }
+
+            candidate += 2
+
+      }
+
+      return multiple
 
 }
+
+console.log(smallestMultiple([1, 10]))
+console.log(smallestMultiple([1, 20]))
