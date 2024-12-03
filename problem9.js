7918d0a6efe126f244809202123eb6530cd60bf1
realpolya <ps2780@columbia.edu>
2024-12-02 21:24:13 -0500

    solve problem 9

--- problem9.js
+++ problem9.js
@@ -11,6 +11,7 @@ Find the product abc.
 
 */
 
+/*
 let a;
 let b;
 let c;
@@ -22,7 +23,51 @@ a < b < c;
 c = 1000 - a - b
 c = Math.sqrt(a^2 + b^2)
 Math.sqrt(a^2 + b^2) = 1000 - a - b
+Math.sqrt(a^2 + b^2) + a = 1000 - b
+a = 1000 - b - Math.sqrt(a^2 + b^2)
+*/
 
-a = Math.sqrt(a^2 + b^2) + b - 1000
+const findTriplet = (sum) => {
 
+      let a;
+      let b;
+      let c;
 
+      // outer loop b
+      bLoop: for (let i = 2; i < +sum/2; i++) {
+
+            // inner loop a
+            for (let j = 1; j < i; j++) {
+
+                  // if the equation above is satisfied and a smaller than b
+                  if (j === (+sum - i - Math.sqrt((j ** 2) + (i ** 2)))) {
+                        
+                        // assign a and b
+                        a = j
+                        b = i
+            
+                        // the same solution with break out of both loops
+                        break bLoop
+                  }
+
+            }
+            
+      }
+
+      
+      // assign c
+      if (a) {
+
+            c = +sum - a - b
+
+            if (a ** 2 + b ** 2 === c ** 2) {
+                  return (a * b * c)
+            }
+
+      }
+
+      return "Solution not found"
+
+}
+
+console.log("Answer to problem 9: ", findTriplet(1000))
