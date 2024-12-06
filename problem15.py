f494a961dd16dcd3b381b301078be395b38a19e5
realpolya <ps2780@columbia.edu>
2024-12-05 21:37:38 -0500

    start problem 16

--- problem15.py
+++ problem15.py
@@ -1,8 +1,26 @@
 '''
 Lattice Paths
 
-Starting in the top left corner of a 2 * 2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+Starting in the top left corner of a 2 x 2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 
-How many such routes are there through a 20 * 20 grid?
+How many such routes are there through a 20 x 20 grid?
 
 '''
+
+import math
+
+def find_routes(size):
+
+    # implement binomial coefficient
+    # n - total number of items
+    # k - number of items to choose (number of decisions we have to make in order to arrive at the end point)
+
+    n = size * 2
+    k = size
+
+    paths = int(math.factorial(n) / (math.factorial(k) ** 2))
+
+    return paths
+
+print(find_routes(4))
+
