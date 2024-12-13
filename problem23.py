a3b27b051274906bef9bbd4b51a6d466c51d03c3
realpolya <ps2780@columbia.edu>
2024-12-13 09:38:14 -0500

    work on problem 23

--- problem23.py
+++ problem23.py
@@ -1,12 +1,35 @@
 '''
-Names Scores
+Non-Abundant Sums
 
-Using names.txt, a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. 
-Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 
+28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
 
-For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. 
-So, COLIN would obtain a score of 938 * 53 = 49714.
-What is the total of all the name scores in the file?
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. 
+By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
+However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of 
+two abundant numbers is less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+Wikipedia: 20161 is the limit
 
 '''
 
+def is_abundant(num):
+
+    divisors = []
+    sum = 0
+
+    # find all of the divisors
+    for i in range(1, num):
+
+        if num % i == 0:
+            divisors.append(i)
+            sum += i
+    
+    return sum > num
+    
+
+
+print(is_abundant(12))
