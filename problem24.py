121c75885f321365f73689b13dd491af6f0870fc
realpolya <ps2780@columbia.edu>
2024-12-16 13:50:50 -0500

    solve problem 24

--- problem24.py
+++ problem24.py
@@ -11,13 +11,72 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 
 '''
 
+import math
+# from itertools import permutations
+
+def permutate(array):
+
+    if len(array) == 1:
+        print("Base case reached: ", array)
+        return str(array[0])
+
+    permutations = []
+    answer = False
+
+    # number of permutations equals to factorial of length
+    perm_num = math.factorial(len(array))
+
+    for n in range(len(array)):
+
+        perm_start = array[n]
+
+        too_long = False
+
+        print("perm start is ", perm_start)
+
+        remaining = array[:n] + array[n+1:]
+        print("remaining is ", remaining)
+
+        for p in permutate(remaining):
+
+            print("doing recursion on ", p)
+            string = "".join(str(perm_start) + str(p))
+            permutations.append(string)
+
+    
+            if len(permutations) >= 1000000:
+
+                too_long = True
+                break
+
+        if too_long:
+
+            break
+
+    return permutations
+
+
+
+
+
 def lexi_perm(limit):
 
     digits = []
+    permutations = []
 
     for i in range(limit+1):
         digits.append(i)
     
-    print(digits)
+    # print(digits)
 
-lexi_perm(9)
+    perms = permutate(digits)
+
+    # print(perms)
+
+    return perms[999999]
+
+
+
+
+
+print("answer is", lexi_perm(9))
