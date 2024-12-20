5ac1c10a063f41945089c75aef009f9a060e0424
realpolya <ps2780@columbia.edu>
2024-12-20 14:49:05 -0500

    work on problem26

--- problem26.py
+++ problem26.py
@@ -5,8 +5,8 @@ A unit fraction contains 1 in the numerator. The decimal representation of the u
 with denominators 2 to 10 are given:
 
 1/2 = 0.5
-1/3 =0.(3)
-1/4 =0.25
+1/3 = 0.(3)
+1/4 = 0.25
 1/5 = 0.2
 1/6 = 0.1(6)
 1/7 = 0.(142857)
@@ -19,3 +19,21 @@ Find the value of d < 1000 for which 1/d contains the longest recurring cycle in
 
 '''
 
+def find_cycle(limit):
+
+    repeat_floats = []
+
+    # for loop
+    for n in range(1, limit + 1):
+
+        # calculate float
+        current = 1 / int(n)
+
+        # in recurring cycles remainders in decimal start to repeat
+        # if 
+
+        # if remainder is 0, skip iteration
+
+        # create a set of remainders
+
+        # while remainder keeps changing, keep going
