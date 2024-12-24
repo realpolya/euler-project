ffef95d48560bd7fa7ae625f3d45f97fb9501642
realpolya <ps2780@columbia.edu>
2024-12-24 08:40:19 -0500

    start problem 27

--- problem26.py
+++ problem26.py
@@ -17,9 +17,11 @@ with denominators 2 to 10 are given:
 Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
 Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
 
+TODO: fix the solution below
+
 '''
 
-def find_cycle(limit):
+def find_cycle(limit=10):
 
     repeat_floats = []
     winner = False
