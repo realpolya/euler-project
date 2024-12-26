ed4ebc6039b0b3fba2af23aa643fd4412732a7c6
realpolya <ps2780@columbia.edu>
2024-12-26 14:25:55 -0500

    work on problem28

--- problem28.py
+++ problem28.py
@@ -14,5 +14,32 @@ It can be verified that the sum of the numbers on the diagonals is 101.
 What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral 
 formed in the same way?
 
-
 '''
+import math
+
+def spiral(size=int(5)):
+
+    matrix = {} # 5x5, each row is represented as a key
+    mid = int(size/2)
+
+    for r in range(size):
+
+        matrix[r] = [] # or set?
+
+        for i in range(size):
+            matrix[r].append("")
+    
+    matrix[mid][mid] = 1
+    print(matrix)
+    
+
+    # for spiral in range(int(size / 2) + 1):
+
+    #     print(math.ceil(size/2))
+
+    
+
+
+spiral()
+
+
