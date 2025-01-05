15749dfc0cdd98ef2290031072aac307124722e0
realpolya <ps2780@columbia.edu>
2025-01-05 08:12:37 -0500

    work on problem33

--- problem33.py
+++ problem33.py
@@ -1,13 +1,34 @@
 '''
 Digit Cancelling Fractions
 
-The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly believe that 49/98 = 4/8, 
-which is correct, is obtained by cancelling the 9s.
+The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly 
+believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
 
 We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
 
-There are exactly four non-trivial examples of this type of fraction, less than one in value, and containing two digits in the numerator and denominator.
+There are exactly four non-trivial examples of this type of fraction, less than one in value, and containing 
+two digits in the numerator and denominator.
+
 If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
 
 '''
 
+def cancel_fraction():
+
+    fractions = []
+
+    for numerator in range(10, 100):
+
+        for denominator in range(10, 100):
+
+            # see if it has the same digit, if not, continue
+            overlap = any(char in str(denominator) for char in str(numerator))
+            
+            if not overlap:
+                continue
+            
+            fractions.append([numerator, "/", denominator])
+    
+    return fractions
+
+print(cancel_fraction())
