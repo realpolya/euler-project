f4168eac9f705964a9e4edcda5200c43a1154fb4
realpolya <ps2780@columbia.edu>
2025-01-08 08:38:40 -0500

    solve problem 36

--- problem36.py
+++ problem36.py
@@ -7,3 +7,54 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 
 '''
 
+import math
+
+
+def is_palindromic(num):
+
+    string = str(num)
+
+    if len(string) == 1:
+        return True
+
+    midpoint = False
+    first_half = False
+    second_half = False
+
+    if len(string) % 2 == 0:
+        midpoint = len(string) // 2
+        first_half = string[:midpoint]
+        second_half = string[midpoint:]
+    else:
+        midpoint = math.ceil(len(string) / 2) - 1
+        first_half = string[:midpoint]
+        second_half = string[midpoint + 1:]
+
+    # print ("first half is ", first_half, "second half is ", second_half)
+    reverse_second = ''.join(reversed(second_half))
+    if first_half == reverse_second:
+        return True
+
+    return False
+
+
+def base_palindromes(limit=1000000):
+
+    sum = 0
+
+    for n in range(1, limit):
+
+        if is_palindromic(n):
+
+            # convert to binary
+            binary_n = bin(n)[2:]
+
+            if is_palindromic(binary_n):
+
+                print("n", n, "is palindromic. binary n is ", binary_n)
+
+                sum += n
+    
+    return sum
+
+print(base_palindromes())
