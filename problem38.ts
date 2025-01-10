9b4880328a17cef7ada2126c37fa96a0000b70fb
realpolya <ps2780@columbia.edu>
2025-01-10 11:12:49 -0500

    work on problem 38

--- problem38.ts
+++ problem38.ts
@@ -9,6 +9,7 @@ Take the number 192 and multiply it by each of 1, 2, and 3:
 192 * 3 = 576
 
 By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3).
+
 The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, 
 giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
 
@@ -16,9 +17,53 @@ What is the largest 1 to 9 pandigital 9-digit number that can be formed as the c
 
 */
 
+const findLargest = (n: number, limit: number): number => {
+
+    let exceeded: boolean = false;
+    let largest: number = 0;
+
+    while (!exceeded) {
+
+        let iArray: number[] = [] 
+
+        for (let i: number = 1; i <= 9; i++) {
+
+            iArray.push(i)
+
+            let products: string = iArray.map(member => {
+                return member * n;
+            }).join('')
+
+            if (products.length > 9) {
+                exceeded = true
+                break
+            } else if (products.length < 9) {
+                continue
+            }
+
+            if (Number(products) > largest) {
+                largest = Number(products)
+            }
+
+        }
+
+    }
+
+    return largest
+
+}
+
+
+
 const panMultiples = (limit: number): number => {
 
     let largest: number = 0;
+
+    // unknown n: integer
+    // unknown set with unknown number of members: consecutive 1 through n
+    // limit 999 999 999 - 9 digits
+
+    
 
     return largest
 
