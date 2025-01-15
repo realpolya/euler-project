d86212be1ff3ec30322e9619b89f37ec8366e956
realpolya <ps2780@columbia.edu>
2025-01-14 23:54:59 -0500

    solve problem 42

--- problem42.py
+++ problem42.py
@@ -4,6 +4,8 @@ Coded Triangle Numbers
 The n-th term of the sequence of triangle numbers is given by,
 
 t_n = 1/2 * n * (n+1); 
+t * 2 = n * (n + 1)
+t * 2 = n^2 + n
 
 so the first ten triangle numbers are:
 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
@@ -20,3 +22,48 @@ containing nearly two-thousand common English words,
 how many are triangle words?
 
 '''
+
+from extras.words42 import WORDS
+from extras.alphabet import ALPHABET
+
+def calculate_value(word, alphabet):
+
+    value = 0
+
+    for char in word:
+        value += alphabet[char] 
+    
+    return value
+
+
+
+def coded_triange_numbers(words=WORDS):
+
+    # formula for triangle
+    # 1/2 * n * (n + 1)
+
+    word_count = 0
+    max_value = 0
+    triangles = []
+
+    for word in words:
+        word_value = calculate_value(word, ALPHABET)
+        if word_value > max_value:
+            max_value = word_value
+    
+    n = 1
+    current_triangle = 0
+
+    while current_triangle < max_value:
+        current_triangle = 1/2 * n * (n + 1)
+        triangles.append(int(current_triangle))
+        n += 1
+
+    for word in words:
+        word_value = calculate_value(word, ALPHABET)
+        if word_value in triangles:
+            word_count += 1
+
+    return word_count
+
+print("Answer to problem 42: ", coded_triange_numbers())
