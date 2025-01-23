ec97f2a5dd68d78bd0caebc534ab91665f1239b3
realpolya <ps2780@columbia.edu>
2025-01-23 06:50:54 -0500

    start problem 47

---
+++ problem47.py
@@ -0,0 +1,24 @@
+'''
+Distinct Primes Factors
+
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 * 7
+15 = 3 * 5.
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2^2 * 7 * 23
+645 = 3 * 5 * 43
+646 = 2 * 17 * 19.
+
+Find the first four consecutive integers to have four distinct prime factors each. 
+What is the first of these numbers?
+
+'''
+
+from extras.utils import is_prime
+
+def distinct_factors(requested=2):
+
+    
