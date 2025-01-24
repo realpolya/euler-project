dcc1499e939805edcd74b68eb1dee2f1d7fed727
realpolya <ps2780@columbia.edu>
2025-01-24 07:56:29 -0500

    solve problem 48

--- problem48.py
+++ problem48.py
@@ -7,4 +7,23 @@ Find the last ten digits of the series,
 
 '''
 
+def self_powers(limit):
 
+    ten_digits = ''
+    sum = 0
+
+    for n in range(1, limit + 1):
+
+        sum += n ** n
+
+    sum_list = list(str(sum))
+
+    for i in range(-10, 0, 1):
+
+        ten_digits += sum_list[i]
+    
+    return ten_digits
+
+
+
+print("Answer to problem 48: ", self_powers(1000))
