a6396e03ac0c3c92779a12ac359fccf98b61ee4a
realpolya <ps2780@columbia.edu>
2025-01-25 12:47:19 -0500

    solve problem 49

--- problem49.py
+++ problem49.py
@@ -18,36 +18,73 @@ three terms in this sequence?
 
 from extras.utils import is_prime, permutate
 
+
 def find_permutations():
 
     final_sequence = ""
+    found = False
 
     for n in range(1000, 9999):
 
         if is_prime(n):
 
-            # check if there are at least two other 
-            # prime permutations
-            # in the set
-
-            n_permutations = permutate(list(str(n)))
-            prime_perms = [n]
+            n_permutations = [int(n) for n in permutate(list(str(n)))]
+            prime_perms = []
             counter = 0
 
-            for perm in n_permutations:
+            if 1487 in set(n_permutations):
+                continue
 
-                if is_prime(perm):
+            for perm in set(n_permutations):
+
+                if is_prime(perm) and len(str(perm)) > 3:
 
                     prime_perms.append(perm)
                     counter += 1
-            
+
             if counter < 2:
 
                 continue
             
             prime_perms.sort()
+        
+            differences = []
+            repeating_diff = []
 
-            if 
+            for i, prime in enumerate(prime_perms):
+
+                remaining = prime_perms[i+1:]
+
+                for second_prime in remaining:
+
+                    difference = abs(prime - second_prime)
+
+                    if difference in differences:
+                        repeating_diff.append(difference)
+
+                    if difference != 0:
+                        differences.append(difference)
+            
+
+            for num in sorted(repeating_diff):
+
+                twice = num * 2
+
+                for prime in prime_perms:
+
+                    if (prime + twice) in prime_perms and (prime + num) in prime_perms:
+
+                        found = True
+                        final_sequence += str(prime)
+                        final_sequence += str(prime + num)
+                        final_sequence += str(prime + twice)
+                
+        if found:
+
+            break
+
+    
+    return final_sequence
 
 
-
+print("Answer to problem 49: ",find_permutations())
