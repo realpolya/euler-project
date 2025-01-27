d5a00cc93e73f6a4898f596431352d622c62c9e0
realpolya <ps2780@columbia.edu>
2025-01-27 17:05:43 -0500

    work on problem 50

--- problem50.py
+++ problem50.py
@@ -14,5 +14,31 @@ consecutive primes?
 
 from extras.utils import is_prime
 
+
 def consecutive_prime_sum(limit=100):
     
+    primes = [2]
+    
+    max_sum = 2 # answer
+    current_sum = 2
+    consecutive_primes = 0 # number of consecutive primes
+
+    # cycle through numbers up to limit
+    for n in range(3, limit, 2):
+
+        # if prime, add to primes
+        if is_prime(n):
+            primes.add(n)
+
+            # add to current sum
+            current_sum += n
+
+            # if sum is not prime, break the sequence ?
+            if not is_prime(current_sum):
+                current_sum = 0
+
+            # if current_sum surpasses max_sum, reassign
+            if current_sum > max_sum:
+
+    
+    return max_sum
