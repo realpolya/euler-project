a19b6f122bc70f61437f82d1b3ff5af522333b66
realpolya <ps2780@columbia.edu>
2025-01-28 08:05:38 -0500

    write first layer of solution for 2 digit numbers

--- problem51.py
+++ problem51.py
@@ -19,33 +19,76 @@ eight prime value family.
 
 from extras.utils import is_prime
 
-def digit_replacement(quantity):
+
+def digit_replacement(quantity=6):
 
     # find the first primes from the permutations
     # if multiple spaces are occupied, they are occupied with the same digit
     answer_sequence = []
+    found = False
+
+    n = 1 # can be beginning or end
+    # we can have m number of n's (as we do not know how 
+    # many parts of the number are permanent)
+    # iterable i can be anything
+
+    # how many digits are in a number?
+    # should we cycle through primes? TODO:
 
     # while loop
-    
-        # loop through numbers
+    while not found:
+    # while n < 10:
 
-        # if is_prime
+        # establish a number of variations
+        # for 2 digits, 10 variations
+        variations = 10
 
-            # establish a number of variations
+        # how many can be non-primes?
+        cutoff = variations - quantity
 
-            # start replacing digits with digits from 0 to 9
+        # n can be in front or behind
+        # start concatenating digits with digits from 0 to 9
+        for front_back in range(2):
+
+            prime_count = 0
+            non_prime_count = 0
+            
+            sequence = []
+
+            for i in range(10):
+
+                # print("")
+                
+                if front_back == 0: # first iteration
+                    current = int(str(n) + str(i))
+                else: # second iteration
+                    if i == 0:
+                        continue
+                    current = int(str(i) + str(n))
 
                 # count primes
+                if is_prime(current):
+                    prime_count += 1
+                    sequence.append(current)
+                else:
+                    non_prime_count += 1
 
                 # if number of primes is not meeting the quantity as compared to number of variations
-
+                if non_prime_count > cutoff:
                     # abandon this route
+                    break
 
-                # if quantity is met
-
+                # if quantity is met - the first instance
+                if prime_count == quantity:
+                    
                     # assign to winning sequence
+                    answer_sequence = sequence
+                    found = True
+                    
+        
+        n += 1
 
     # return member 0 of winning sequence
     return answer_sequence
 
-                
+print(digit_replacement())
