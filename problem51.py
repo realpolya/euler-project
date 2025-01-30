0486e89e98ff40f27cada5ce3f118dfc56a1d7b5
realpolya <ps2780@columbia.edu>
2025-01-30 15:34:45 -0500

    work on problem 51

--- problem51.py
+++ problem51.py
@@ -24,67 +24,81 @@ def digit_replacement(quantity=6):
 
     # find the first primes from the permutations
     # if multiple spaces are occupied, they are occupied with the same digit
+
     answer_sequence = []
+    not_sequence = [] # storage for numbers that are already filtered out
     found = False
+    family_size = 10 # there are always only 10 digits to iterate over
+    cutoff = family_size - quantity # no point digging deeper beyond this point
 
-    n = 1 # can be beginning or end
-    # we can have m number of n's (as we do not know how 
-    # many parts of the number are permanent)
-    # iterable i can be anything
+    n = 10
 
-    # how many digits are in a number?
-    # should we cycle through primes? TODO:
+    # cycle through primes
 
-    # while loop
     while not found:
     # while n < 10:
+        if is_prime(n):
 
-        # establish a number of variations
-        # for 2 digits, 10 variations
-        variations = 10
+            # count digits
+            digit_num = len(str(n))
+            digit_indices = {}
+            special_case = False # numbers with repeating digits
 
-        # how many can be non-primes?
-        cutoff = variations - quantity
-
-        # n can be in front or behind
-        # start concatenating digits with digits from 0 to 9
-        for front_back in range(2):
-
-            prime_count = 0
-            non_prime_count = 0
+            # variations are based on number of digits and repeating digits
+            variations = digit_num # base (nmx, nxm, xnm)
             
-            sequence = []
+            if digit_num != len(set(str(n))) and digit_num > 2:
 
-            for i in range(10):
+                special_case = True
 
-                # print("")
+                for i, char in enumerate(str(n)):
+
+                    if char in digit_indices:
+                        digit_indices[char].append(i)
+                    else:
+                        digit_indices[char] = [i]
                 
-                if front_back == 0: # first iteration
-                    current = int(str(n) + str(i))
-                else: # second iteration
-                    if i == 0:
-                        continue
-                    current = int(str(i) + str(n))
+                repeating_digits = {digit: indices for digit, indices in digit_indices.items() if len(indices) > 1}
 
-                # count primes
-                if is_prime(current):
-                    prime_count += 1
-                    sequence.append(current)
-                else:
-                    non_prime_count += 1
+            # FIXME: below
+            # every constant number is labeled anything but x (i.e. n, m, j....)
+            for front_back in range(2):
 
-                # if number of primes is not meeting the quantity as compared to number of variations
-                if non_prime_count > cutoff:
-                    # abandon this route
-                    break
+                prime_count = 0
+                non_prime_count = 0
+                
+                sequence = []
 
-                # if quantity is met - the first instance
-                if prime_count == quantity:
+                for i in range(10):
+
+                    # print("")
                     
-                    # assign to winning sequence
-                    answer_sequence = sequence
-                    found = True
-                    
+                    if front_back == 0: # first iteration
+                        current = int(str(n) + str(i))
+                    else: # second iteration
+                        if i == 0:
+                            continue
+                        current = int(str(i) + str(n))
+
+                    # count primes
+                    if is_prime(current):
+                        prime_count += 1
+                        sequence.append(current)
+                    else:
+                        non_prime_count += 1
+
+                    # if number of primes is not meeting the quantity as compared to number of variations
+                    if non_prime_count > cutoff:
+                        # abandon this route
+                        break
+
+                    # if quantity is met - the first instance
+                    if prime_count == quantity:
+                        
+                        # assign to winning sequence
+                        answer_sequence = sequence
+                        found = True
+                        
         
         n += 1
 
@@ -92,3 +106,57 @@ def digit_replacement(quantity=6):
     return answer_sequence
 
 print(digit_replacement())
+
+
+# OLD SOLUTION
+# while not found:
+#     # while n < 10:
+
+#         # establish a number of variations
+#         # for 2 digits, 10 variations
+#         variations = 10
+
+#         # how many can be non-primes?
+#         cutoff = variations - quantity
+
+#         # n can be in front or behind
+#         # start concatenating digits with digits from 0 to 9
+#         for front_back in range(2):
+
+#             prime_count = 0
+#             non_prime_count = 0
+            
+#             sequence = []
+
+#             for i in range(10):
+
+#                 # print("")
+                
+#                 if front_back == 0: # first iteration
+#                     current = int(str(n) + str(i))
+#                 else: # second iteration
+#                     if i == 0:
+#                         continue
+#                     current = int(str(i) + str(n))
+
+#                 # count primes
+#                 if is_prime(current):
+#                     prime_count += 1
+#                     sequence.append(current)
+#                 else:
+#                     non_prime_count += 1
+
+#                 # if number of primes is not meeting the quantity as compared to number of variations
+#                 if non_prime_count > cutoff:
+#                     # abandon this route
+#                     break
+
+#                 # if quantity is met - the first instance
+#                 if prime_count == quantity:
+                    
+#                     # assign to winning sequence
+#                     answer_sequence = sequence
+#                     found = True
+                    
+        
+#         n += 1
