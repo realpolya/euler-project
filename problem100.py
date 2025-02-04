8ae004f918b6415681a84296b838c3d450c77436
realpolya <ps2780@columbia.edu>
2025-02-04 12:53:45 -0800

    update solution for 100

--- problem100.py
+++ problem100.py
@@ -15,15 +15,131 @@ discs in total, determine the number of blue discs that the box would contain.
 
 '''
 
-# the solution is about generating the next one, not cycling over
-# the numbers are too large to cycle over
-# quadratic Diophantine equation
-
 import math
 from fractions import Fraction
 
 
-''' 
+def number_of_blues(frac_b, frac_n, frac_bs, frac_ns):
+    '''get two fractions, find the smallest integer values of B and N'''
+
+    # B = frac_b * k
+    # N = frac_n * k
+
+    # B - 1 = frac_bs * m
+    # N - 1 = frac_ns * m
+
+    # k = (frac_bs * m + 1) / frac_b
+    # k = (frac_ns * m + 1) / frac_n
+
+    # frac_b * (frac_ns * m + 1) = (frac_bs * m + 1) * frac_n
+    # frac_b * frac_ns * m + frac_b = frac_bs * m * frac_n + frac_n
+    # frac_b * frac_ns * m - frac_bs * m * frac_n = frac_n - frac_b
+    # m = (frac_n - frac_b) / (frac_b * frac_ns - frac_bs * frac_n)
+
+    m = (frac_n - frac_b) / (frac_b * frac_ns - frac_bs * frac_n)
+    k = (frac_bs * m + 1) / frac_b
+
+    B = frac_b * k
+    N = frac_n * k
+
+    if N > 1000000000000:
+        return B
+
+    return False
+
+
+
+def create_sequence(b1, n1, b1s, n1s): #s for smaller
+
+    b_list = [b1]
+    n_list = [n1]
+    bs_list = [b1s, b1 + b1s]
+    ns_list = [n1s, n1 + n1s]
+    B = False
+
+    alternate = True
+    counter = 0
+    i = 0
+
+    while not B:
+
+        if alternate:
+
+            if counter == 0:
+
+                # print("b list is ", b_list, "index is ", i)
+                next_b = b_list[i] + bs_list[i+1]
+                b_list.append(next_b)
+                next_n = n_list[i] + ns_list[i+1]
+                n_list.append(next_n)
+
+                B = number_of_blues(b_list[i+1], n_list[i+1], bs_list[i+1], ns_list[i+1])
+
+                counter += 1
+            
+            elif counter == 1:
+
+                next_b = b_list[i] + bs_list[i]
+                b_list.append(next_b)
+                next_n = n_list[i] + ns_list[i]
+                n_list.append(next_n)
+                counter = 0
+
+                alternate = False
+
+        else:
+            
+            if counter == 0:
+
+                next_bs = b_list[i] + bs_list[i-1]
+                bs_list.append(next_bs)
+                next_ns = n_list[i] + ns_list[i-1]
+                ns_list.append(next_ns)
+
+                B = number_of_blues(b_list[i], n_list[i], bs_list[i], ns_list[i])
+
+                counter += 1
+            
+            elif counter == 1:
+
+                i -= 2
+                next_bs = b_list[i+1] + bs_list[i+1]
+                bs_list.append(next_bs)
+                next_ns = n_list[i+1] + ns_list[i+1]
+                ns_list.append(next_ns)
+                counter = 0
+
+                alternate = True
+        
+        i += 1
+
+    return B
+
+
+
+
+def arranged_probability():
+
+    answer = int(create_sequence(5, 7, 7, 10))
+
+    return answer
+
+
+
+print("Answer to problem 100: ", arranged_probability())
+
+
+
+
+
+'''
+
+Solution observations:
+
+# the solution is about generating the next one, not cycling over
+# the numbers are too large to cycle over
+# quadratic Diophantine equation
+
 blue are  15
 B/N =  5/7 and B-1/N-1 =  7/10
 blue are  85
@@ -71,140 +187,4 @@ N2 - 1num + N3num = N3 - 1num
 N3 - 1num + N3num = N4 - 1num
 N4 - 1num + N5num = N5 - 1num
 
-
 '''
-
-def number_of_blues(frac_b, frac_n, frac_bs, frac_ns):
-    '''get two fractions, find the smallest integer values of B and N'''
-    # B = frac_b * k
-    # N = frac_n * k
-
-    # B - 1 = frac_bs * m
-    # N - 1 = frac_ns * m
-
-    # k = (frac_bs * m + 1) / frac_b
-    # k = (frac_ns * m + 1) / frac_n
-
-    # frac_b * (frac_ns * m + 1) = (frac_bs * m + 1) * frac_n
-    # frac_b * frac_ns * m + frac_b = frac_bs * m * frac_n + frac_n
-    # frac_b * frac_ns * m - frac_bs * m * frac_n = frac_n - frac_b
-    # m = (frac_n - frac_b) / (frac_b * frac_ns - frac_bs * frac_n)
-
-    m = (frac_n - frac_b) / (frac_b * frac_ns - frac_bs * frac_n)
-    k = (frac_bs * m + 1) / frac_b
-
-    B = frac_b * k
-    N = frac_n * k
-
-    Bs = frac_bs * m
-    Ns = frac_ns * m
-
-    print("B/N", B, "/", N)
-    print("B-1/N-1", Bs, "/", Ns)
-
-    if N > 1000000000000:
-        return B
-
-    return False
-
-
-
-def create_sequence(b1, n1, b1s, n1s, limit): #s for smaller
-
-    b_list = [b1]
-    n_list = [n1]
-    bs_list = [b1s, b1 + b1s]
-    ns_list = [n1s, n1 + n1s]
-
-    alternate = True
-    counter = 0
-    i = 0
-
-    while i < limit:
-
-        if alternate:
-
-            if counter == 0:
-
-                # print("b list is ", b_list, "index is ", i)
-                next_b = b_list[i] + bs_list[i+1]
-                b_list.append(next_b)
-                next_n = n_list[i] + ns_list[i+1]
-                n_list.append(next_n)
-                counter += 1
-            
-            elif counter == 1:
-
-                next_b = b_list[i] + bs_list[i]
-                b_list.append(next_b)
-                next_n = n_list[i] + ns_list[i]
-                n_list.append(next_n)
-                counter = 0
-
-                alternate = False
-
-        else:
-            
-            if counter == 0:
-
-                next_bs = b_list[i] + bs_list[i-1]
-                bs_list.append(next_bs)
-                next_ns = n_list[i] + ns_list[i-1]
-                ns_list.append(next_ns)
-                counter += 1
-            
-            elif counter == 1:
-
-                i -= 2
-                next_bs = b_list[i+1] + bs_list[i+1]
-                bs_list.append(next_bs)
-                next_ns = n_list[i+1] + ns_list[i+1]
-                ns_list.append(next_ns)
-                counter = 0
-
-                alternate = True
-        
-        i += 1
-    
-    sequence = []
-    sequence_s = []
-
-    for i in range(len(b_list)):
-        sequence.append(Fraction(b_list[i], n_list[i]))
-
-    for i in range(len(bs_list)):
-        sequence_s.append(Fraction(bs_list[i], ns_list[i]))
-    
-    if len(b_list) > len(bs_list):
-        length = len(bs_list)
-    else:
-        length = len(b_list)
-
-    for i in range(length):
-        B = number_of_blues(b_list[i], n_list[i], bs_list[i], ns_list[i])
-        if B:
-            return B
-    
-    return False
-    
-    # print("b/n sequence")
-    # for frac in sequence:
-    #     print(frac)
-    
-    # print("b-1/n-1 sequence")
-    # for frac in sequence_s:
-    #     print(frac)
-
-
-def arranged_probability():
-
-    answer = False
-    i = 2
-
-    while not answer:
-        answer = create_sequence(5, 7, 7, 10, i)
-        i += 1
-    
-    return answer
-
-print(int(arranged_probability()))
