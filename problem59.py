cbcd013ead3c5fffdfc883304b70aab7f4ab1d68
realpolya <ps2780@columbia.edu>
2025-03-05 16:14:19 -0500

    work on the solution for problem 59

--- problem59.py
+++ problem59.py
@@ -30,12 +30,18 @@ decrypt the message and find the sum of the ASCII values in the original text.
 
 import string
 import enchant
+import nltk
+from nltk.corpus import words
 from extras.xor59 import CIPHER
+
+# print(enchant.list_dicts())
+nltk.download("words")
+
 
 def decrypt(encrypted, key):
 
-    decrypted = encrypted ^ key # XOR operation
-    return chr(decrypted)
+    decrypted = int(encrypted) ^ int(key) # XOR operation
+    return decrypted
 
 
 def xor_decryption():
@@ -43,11 +49,78 @@ def xor_decryption():
     # find the sum of ASCII values in the original text
     ascii_sum = 0
 
-    # introduce spell-checking
-    dict = enchant.Dict("en_US")
-    print(dict.check("wonderful"))
+    cipher_list = CIPHER.split(",")
+    # print(cipher_list)
 
-    # loop through possible keys
+    # introduce spell-checking
+    dict = enchant.Dict("en_US-wo_accents")
+    # aspell_dict = enchant.DictWithPWL("en_US", "/Users/polinastepanova/.aspell.en.pws")
+    # print(dict.check("wonderful"))
+
+    english_words = set(words.words())
+    # print(english_words)
+    # print("xyz" in english_words)
+
+    # space is ascii 32, letters 65-122
+    upper_range = range(65, 91)
+    lower_range = range(97, 123)
+    correct_key = []
+
+    # print(dict.check("sf"))
+
+
+    # loop through possible keys ascii lowercase 97 through 122
+    for a in range(97, 123):
+
+        for b in range(97, 123):
+
+            for c in range(97, 123):
+
+                # establish the key sequence
+                keys = [a, b, c]
+                i = 0
+                word_list = []
+                decrypted = ""
+                not_key = False
+
+                # apply the sequence over and over again
+                for char in cipher_list:
+
+                    decrypted_char = decrypt(char, keys[i])
+
+                    # analyze the char
+                    if decrypted_char in upper_range or decrypted_char in lower_range: # adding letters to the word
+                        decrypted += chr(decrypted_char)
+                        # print(decrypted)
+                    # signals break in a word
+                    elif len(decrypted) != 0:
+
+                        # if single letter - only I and a are allowed
+                        if len(decrypted) == 1 and decrypted_char in [65, 97, 49]: # single-letter word, a, A, I 
+                            # print("appending ", decrypted)
+                            word_list.append(decrypted)
+                            decrypted = ""
+                        # elif not dict.check(decrypted):
+                        # elif dict.check(decrypted) and len(decrypted) > 1:
+                        elif decrypted in english_words and len(decrypted) > 1:
+                            # print("key is", keys, "appending ", decrypted)
+                            word_list.append(decrypted)
+                            decrypted = ""
+                        else:
+                            not_key = True
+                            break # this key combo is not the answer
+
+
+                    if i == 2:
+                        i = 0
+                    else:
+                        i += 1
+
+                if not_key:
+                    continue
+                
+                correct_key = [a, b, c]
+                print(word_list)
 
 
         # once it starts returning actual english words
