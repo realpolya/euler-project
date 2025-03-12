22acd73de237238acca7ff6a4f20ad64fa6ed91e
realpolya <ps2780@columbia.edu>
2025-03-12 16:52:25 -0400

    optimize solution for problem 60

--- problem60.py
+++ problem60.py
@@ -12,9 +12,13 @@ Find the lowest sum for a set of five primes for which any two primes concatenat
 '''
 
 import math
+import time
+
 from extras.utils import is_prime
 
-def find_candidates(member_1, member_2, sets_1, sets_2, pairs, four_subsets, limit, candidates=None):
+concat_prime_cache = {}
+
+def find_candidates(member_1, member_2, sets_1, sets_2, dict, limit, candidates=None):
 
     # print("dealing with set_1", sets_1, "and set_2", sets_2)
 
@@ -27,8 +31,10 @@ def find_candidates(member_1, member_2, sets_1, sets_2, pairs, four_subsets, lim
     
     current_members = {member_1, member_2}
 
+    print("sets 1 is ", sets_1, "sets 2 is ", sets_2)
+
     # leave only those sets that contain the same numbers in both sets (excluding the one with member 1 and 2)
-    overlap = [s1 for s1 in sets_1 if any((s1 & s2) - current_members for s2 in sets_2)]
+    overlap = [num for num in sets_1 if num in sets_2 and num not in current_members]
     # overlap = [s for s in overlap if not (member_1 in s and member_2 in s)]
 
     if len(overlap) == 0:
@@ -36,15 +42,13 @@ def find_candidates(member_1, member_2, sets_1, sets_2, pairs, four_subsets, lim
         return []
 
     new_candidates = [num for s in overlap for num in s if num != member_1]
-    new_candidates = [num for num in new_candidates if num in four_subsets]
 
     # print("new candidates now are ", new_candidates, "limit is ", limit)
     
     for candidate in new_candidates[:]:
 
-        sets_3 = [s for s in pairs if candidate in s]
-        next_candidates = find_candidates(member_1, candidate, overlap, sets_3, pairs, 
-        four_subsets, limit - 1, new_candidates)
+        sets_3 = dict[candidate]
+        next_candidates = find_candidates(member_1, candidate, overlap, sets_3, dict, limit - 1, new_candidates)
         
         if not next_candidates:
             new_candidates.remove(candidate)
@@ -53,13 +57,13 @@ def find_candidates(member_1, member_2, sets_1, sets_2, pairs, four_subsets, lim
 
 
 # find the lowest sum of the five primes for which any two primes concatenate to produce another prime
-def is_concat_prime(num_1, num_2):
+def is_concat_prime(num_1, num_2, primes):
 
     # try concatenating combinations within that set
     concat_1 = int(str(num_1) + str(num_2))
     concat_2 = int(str(num_2) + str(num_1))
 
-    if not is_prime(concat_1) or not is_prime(concat_2):
+    if concat_1 not in primes or concat_2 not in primes:
         return False
     
     return True
@@ -67,113 +71,182 @@ def is_concat_prime(num_1, num_2):
 
 def prime_pair_sets(quantity=4):
 
+    start_time = time.time()
+    # Call function here
+
     lowest_sum = 0
     start = 3
-    limit = 299
-    primes = [2]
-    answer_list = []
-    
+    limit = 100
+    # primes = [2]
 
+    is_prime_cache = [True] * limit # Sieve of Eratosthenes
+    is_prime_cache[0] = is_prime_cache[1] = False # 0 and 1 are not primes
+
+    for num in range(2, math.isqrt(limit) + 1):
+        if is_prime_cache[num]:
+            for multiplied in range(num ** 2, limit, +num):
+                is_prime_cache[multiplied] = False
+    
+    prime_set = {num for num, prime in enumerate(is_prime_cache) if prime}
+    primes = sorted(prime_set)
+
+    large_limit = 1000000
+    large_is_prime_cache = [True] * large_limit # Sieve of Eratosthenes
+    large_is_prime_cache[0] = large_is_prime_cache[1] = False # 0 and 1 are not primes
+
+    for num in range(2, math.isqrt(large_limit) + 1):
+        if large_is_prime_cache[num]:
+            for multiplied in range(num ** 2, large_limit, +num):
+                large_is_prime_cache[multiplied] = False
+    
+    large_prime_set = {num for num, prime in enumerate(large_is_prime_cache) if prime}
+
+    print(f"1. Execution time: {time.time() - start_time:.2f} seconds")
+    start_time = time.time()
+
+
+
+    answer_list = []
     pairs = []
-    quadruples = []
+    pairs_dict = {p: set() for p in primes}
+    # quadruples = []
 
     # while loop
-    while lowest_sum == 0:
+    # while lowest_sum == 0:
 
-        for i in range(start, limit, +2):
-            if is_prime(i):
-                primes.append(i)
-        
-        print("primes length is ", len(primes))
-        index = 0
+    # math.isqrt(prime_limit)
 
-        # work in pairs?
-        for prime in primes:
+    # for i in range(start, limit, +2):
+    #     if is_prime(i):
+    #         primes.append(i)
+    
+    print("primes length is ", len(primes))
+    index = 0
 
-            primes_cut = primes[index+1:]
-            index += 1
+    # work in pairs?
+    for prime in primes:
 
-            for prime_2 in primes_cut:
+        primes_cut = primes[index+1:]
+        index += 1
 
-                if prime == prime_2:
+        for prime_2 in primes_cut:
+
+            if prime == prime_2:
+                continue
+
+            new_pair = {prime, prime_2}
+
+            if new_pair not in pairs:
+
+                # if a combination is not prime, discard that pair
+                if not is_concat_prime(prime, prime_2, large_prime_set):
                     continue
 
-                new_pair = set()
-                new_pair.update([prime, prime_2])
+                # pairs.append(new_pair)
+                pairs_dict[prime].add(prime_2)
+                pairs_dict[prime_2].add(prime)
+                pairs.append(new_pair)
+    
+    print("number of pairs: ", len(pairs))
+    print(f"2. Execution time: {time.time() - start_time:.2f} seconds")
+    start_time = time.time()
 
-                if new_pair not in pairs:
 
-                    # if a combination is not prime, discard that pair
-                    if not is_concat_prime(prime, prime_2):
-                        continue
 
-                    pairs.append(new_pair)
+    # first_member_quantity = quantity - 1
+    four_subsets_dict = {p: candidates for p, candidates in pairs_dict.items() if len(candidates) >= 4}
+    print("four subsets now ", four_subsets_dict)
+    valid_keys = set(four_subsets_dict.keys())
+
+    four_subsets_dict = {p: {v for v in values if v in valid_keys} for p, values in four_subsets_dict.items()}  
+    print("four subsets AFTER", four_subsets_dict)  
+
+
+    four_subset_pairs = []
+
+    for pair in pairs:
+        member_1, member_2 = tuple(pair)
+
+        if member_1 in four_subsets_dict and member_2 in four_subsets_dict:
+            if pair not in four_subset_pairs:
+                four_subset_pairs.append(pair)
+
+
+    print(f"3. Execution time: {time.time() - start_time:.2f} seconds")
+    start_time = time.time()
+
+
+    # for pair in pairs:
+
+    #     # candidate number - is it found in 4 pairs? if not skip, if yes, go through each sets of 4
+    #     # TODO: do not repeat members
+    #     # member_1 = list(pair)[0]
+    #     # member_2 = list(pair)[1]
+
+    #     member_1, member_2 = tuple(pair)
+
+    #     if member_1 in four_subsets and member_2 in four_subsets:
+    #         continue
         
-        print("number of pairs: ", len(pairs))
+    #     # member_1_count = sum(1 for s in pairs if member_1 in s)
+    #     # member_2_count = sum(1 for s in pairs if member_2 in s)
 
-        first_member_quantity = quantity - 1
-        four_subsets = set()
-        four_subset_pairs = []
+    #     member_1_count = len(pairs_dict[member_1])
+    #     member_2_count = len(pairs_dict[member_2])
 
-        for pair in pairs:
 
-            # candidate number - is it found in 4 pairs? if not skip, if yes, go through each sets of 4
-            # TODO: do not repeat members
-            member_1 = list(pair)[0]
-            member_2 = list(pair)[1]
-
-            if member_1 in four_subsets and member_2 in four_subsets:
-                continue
-            
-            member_1_count = sum(1 for s in pairs if member_1 in s)
-            member_2_count = sum(1 for s in pairs if member_2 in s)
-
-            if member_1_count < first_member_quantity and member_2_count < first_member_quantity:
-                continue
-            elif member_1_count > first_member_quantity and member_2_count > first_member_quantity:
-                four_subsets.add(member_1)
-                four_subsets.add(member_2)
-                # print("these members are in more than 4 subsets", member_1, member_2)
-                if pair not in four_subset_pairs:
-                    four_subset_pairs.append(pair)
-            elif member_1_count > first_member_quantity:
-                four_subsets.add(member_1)
-                # print("this member is in more than 4 subsets", member_1)
-            elif member_1_count > first_member_quantity:
-                four_subsets.add(member_2)
-                # print("this member is in more than 4 subsets", member_2)
-            
-        print("len of four subsets members", len(four_subsets))
-        print("len of four subset pairs", len(four_subset_pairs))
-
-        # all of numbers in the pair must be in 4 pairs
+    #     # if member_1_count < first_member_quantity and member_2_count < first_member_quantity:
+    #     #     continue
+    #     # elif member_1_count > first_member_quantity and member_2_count > first_member_quantity:
+    #     #     four_subsets.add(member_1)
+    #     #     four_subsets.add(member_2)
+    #     #     # print("these members are in more than 4 subsets", member_1, member_2)
+    #     #     if pair not in four_subset_pairs:
+    #     #         four_subset_pairs.append(pair)
+    #     # elif member_1_count > first_member_quantity:
+    #     #     four_subsets.add(member_1)
+    #     #     # print("this member is in more than 4 subsets", member_1)
+    #     # elif member_1_count > first_member_quantity:
+    #     #     four_subsets.add(member_2)
+    #     #     # print("this member is in more than 4 subsets", member_2)
         
-        for pair in four_subset_pairs:
+    # print("len of four subsets members", len(four_subsets))
+    # print("len of four subset pairs", len(four_subset_pairs))
 
-            member_1 = list(pair)[0]
-            member_2 = list(pair)[1]
+    # all of numbers in the pair must be in 4 pairs
+    
+    for pair in four_subset_pairs:
 
-            # if member_1 not in four_subsets or member_2 not in four_subsets:
-            #     continue
-            
-            # bring subsets with member_1 in them
-            member_1_subsets = [s for s in pairs if member_1 in s]
-            # bring subsets with member_2 in them
-            member_2_subsets = [s for s in pairs if member_2 in s]
+        # member_1 = list(pair)[0]
+        # member_2 = list(pair)[1]
+        member_1, member_2 = tuple(pair)
 
-            # if member_1 == 19 and member_2 == 31:
+        # if member_1 not in four_subsets or member_2 not in four_subsets:
+        #     continue
+        
+        # # bring subsets with member_1 in them
+        # member_1_subsets = [s for s in pairs if member_1 in s]
+        # # bring subsets with member_2 in them
+        # member_2_subsets = [s for s in pairs if member_2 in s]
 
-            candidates = find_candidates(member_1, member_2, member_1_subsets, member_2_subsets, pairs, four_subsets, 3)
-            if candidates:
-                print("for member_1", member_1, "and member_2", member_2, "candidates are", candidates)
-                lowest_sum = sum(candidates) + member_1 + member_2
-                break
+        member_1_subsets = four_subsets_dict[member_1]
+        member_2_subsets = four_subsets_dict[member_2]
 
-        # recursive function until the answer is found
+        # if member_1 == 19 and member_2 == 31:
 
-        if lowest_sum == 0:
-            start = limit
-            limit += 500
+        candidates = find_candidates(member_1, member_2, member_1_subsets, member_2_subsets, four_subsets_dict, 3)
+        if candidates:
+            print("for member_1", member_1, "and member_2", member_2, "candidates are", candidates)
+            lowest_sum = sum(candidates) + member_1 + member_2
+            break
+    
+    print(f"4. Execution time: {time.time() - start_time:.2f} seconds")
+
+    # recursive function until the answer is found
+
+    # if lowest_sum == 0:
+    #     start = limit
+    #     limit += 500
 
 
     return lowest_sum
--- problem60_extra.py
+++ problem60_extra.py
@@ -1,4 +1,6 @@
-def find_candidates(member_1, member_2, sets_1, sets_2, pairs, four_subsets, limit, candidates=None):
+def find_candidates(member_1, member_2, sets_1, sets_2, dict, limit, candidates=None):
+
+    # print("dealing with set_1", sets_1, "and set_2", sets_2)
 
     if candidates is None:
         candidates = []
@@ -6,21 +8,27 @@ def find_candidates(member_1, member_2, sets_1, sets_2, pairs, four_subsets, lim
     #base case
     if limit == 0:
         return candidates
+    
+    current_members = {member_1, member_2}
+
+    print("sets 1 is ", sets_1, "sets 2 is ", sets_2)
 
     # leave only those sets that contain the same numbers in both sets (excluding the one with member 1 and 2)
-    overlap = [s1 for s1 in sets_1 if any(s1 & s2 for s2 in sets_2)]
-    overlap = [s for s in overlap if not (member_1 in s and member_2 in s)]
+    overlap = [num for num in sets_1 if num in sets_2 and num not in current_members]
+    # overlap = [s for s in overlap if not (member_1 in s and member_2 in s)]
 
     if len(overlap) == 0:
-        return candidates
+        # print("the overlap is 0")
+        return []
 
     new_candidates = [num for s in overlap for num in s if num != member_1]
-    new_candidates = [num for num in new_candidates if num in four_subsets]
+
+    # print("new candidates now are ", new_candidates, "limit is ", limit)
     
     for candidate in new_candidates[:]:
 
-        sets_3 = [s for s in pairs if candidate in s]
-        next_candidates = find_candidates(member_1, candidate, overlap, sets_3, pairs, four_subsets, limit - 1, new_candidates)
+        sets_3 = dict[candidate]
+        next_candidates = find_candidates(member_1, candidate, overlap, sets_3, dict, limit - 1, new_candidates)
         
         if not next_candidates:
             new_candidates.remove(candidate)
