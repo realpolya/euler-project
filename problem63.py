bf1963ec75e885e405dce217af3f4d328fc8a70c
realpolya <ps2780@columbia.edu>
2025-03-24 11:51:23 -0400

    write non-working solutino for probelm 63

--- problem63.py
+++ problem63.py
@@ -8,4 +8,17 @@ How many n-digit positive integers exist which are also an nth power?
 '''
 
 def powerful_digit_counts():
+
+    count = 0
+
+    for num in range(1, 100):
+        for n in range(2, 200):
+            # print("for exponent", n, "number of digis", len(str(5 ** n)))
+            result = num ** n
+            if len(str(result)) == n:
+                print("for num", num, "for exponent", n, "number of digis", len(str(result)), "result is:", result)
+                count += 1
     
+    return count
+    
+print(powerful_digit_counts())
