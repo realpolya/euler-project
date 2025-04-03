d1a24feb1791fc614a0d0ff21944bfadc5d2d0dc
realpolya <ps2780@columbia.edu>
2025-04-03 09:29:04 -0400

    start odd period roots function

--- problem64.py
+++ problem64.py
@@ -9,3 +9,17 @@ import math
 # problem 26 also calculates infinite cycles
 
 # only focus on irrational square roots
+
+def odd_period_roots(limit=13):
+
+    for n in range(2, limit + 1):
+
+        root = math.sqrt(n)
+
+        if root.is_integer():
+            continue
+        
+        a0 = math.floor(root)
+        print(a0)
+    
+odd_period_roots()
