3d3f7ac82a09dc5e32bdaea8cd5e96caeb8f8519
realpolya <ps2780@columbia.edu>
2025-04-08 11:57:47 -0400

    finallygad! solve problem 64

--- problem64.py
+++ problem64.py
@@ -3,17 +3,11 @@ Odd Period Square Roots
 
 '''
 
-from fractions import Fraction
-# from sympy import sqrt, Rational, simplify # library for fractions
 import math
-
-# problem 26 also calculates infinite cycles
-# only focus on irrational square roots
 
 def odd_period_roots(limit=13):
 
     odd_count = 0
-
 
     for n in range(2, limit + 1):
 
@@ -33,38 +27,30 @@ def odd_period_roots(limit=13):
         while period_not_done:
 
             if increment == 1:
-                next_step = (math.sqrt(n) + current_a) / (n - (current_a ** 2))
+                next_step = a0
+                remainder = (n - next_step * next_step)
             else:
-                next_step = 1 / remainder
-                # next_step = 1 / remainder
+                next_step = remainder * floor - next_step
+                remainder = (n - next_step * next_step) // remainder
             
-            floor = math.floor(next_step)
+            floor = (a0 + next_step) // remainder
 
-            # what is left after the next_step? the remainder?
-            remainder = next_step - floor
-            # remainder = next_step - floor
-
-            # print("next step is ", simplify(next_step))
-            # print("remainder is ", remainder)
-
-            triplet = [next_step, floor, remainder]
+            triplet = (next_step, floor, remainder)
 
             if triplet in triplets:
                 period_not_done = False
 
                 if len(period) % 2 != 0:
-                    print("for n", n, "period is ", period)
                     odd_count += 1
+
             else:
                 triplets.add(triplet)
-                # print("for n ", n, "a", increment, " is ", floor)
                 period.append(floor)
 
             current_a = floor
             increment += 1
     
-
     return odd_count
 
     
-print("odd count is: ", odd_period_roots(10000))
+print("Answer to problem 64: ", odd_period_roots(10000))
