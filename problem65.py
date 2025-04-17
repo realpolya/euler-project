b867d5020b17ff12c173c8ddaa685c1b79700db1
realpolya <ps2780@columbia.edu>
2025-04-17 17:31:10 -0400

    done with problem 65

--- problem65.py
+++ problem65.py
@@ -12,46 +12,14 @@ from sympy import simplify, Rational, E
 
 def find_convergents(limit=10):
 
-    # write function for the square root of 2
-
-    # a0 = math.floor(math.sqrt(2))
-    # prev_term = Rational(0)
-
-    # for _ in range(10):
-
-    #     new_term = simplify(1 / (2 + prev_term))
-    #     prev_term = new_term
-
-    #     result = simplify(a0 + new_term)
-
-    #     print(result)
-    
     start = E
     a0 = math.floor(E)
     remainder = (E - a0).evalf()
-    # print("remainder is ", remainder)
-    prev_term = Rational(0)
-    prev_a = 0
     count = 1
-
     a_series = []
-    # for i in range(1, limit):
-    #     if i % 3 == 2:
-    #         a_series.append(2 * ((i + 1) // 3))
-    #     else:
-    #         a_series.append(1)
 
 
     for i in range(1, limit + 1):
-
-        # a = math.floor(1 / remainder)
-        # prev_r = remainder
-        # remainder = simplify((1 / prev_r) - a)
-
-        # # add to a series
-        # a_series.append(a)
-
-        # result = a0
 
         if i % 3 == 2:
             a_series.append(2 * ((i + 1) // 3))
@@ -60,6 +28,7 @@ def find_convergents(limit=10):
 
         # get the last member of a_series, the newest a
         nested_fraction = Rational(a_series[-1])
+
         # take the remaining a members from the series backwards
         for a_num in reversed(a_series[:-1]):
 
@@ -67,64 +36,19 @@ def find_convergents(limit=10):
             nested_fraction = a_num + 1 / nested_fraction
         
         convergent = simplify(a0 + 1 / nested_fraction)
-
-        # print("terms so far:", a0, "and series are ", a_series)
-
-        # print("convergent is ", convergent)
         count += 1
-        print("convergent number ", count, "convergent is ", convergent)
 
         if count == limit:
-            print("length: ", len(a_series))
             numerator, denominator = convergent.as_numer_denom()
             numerator_answer = numerator
-            # print("for count ", count, "numerator is ", numerator)
             break
-            
-        
-    # s = "6992432529593258630263690665699787787145693139"
-    # print("here we go ", sum(int(c) for c in s))  # should print 258
 
     digit_sum = 0
     for char in list(str(numerator_answer)):
         digit_sum += int(char)
-        # print("added ", int(char), ". now sum is ", digit_sum)
 
-    print("a series is ", a_series)
-    print("numerator is ", numerator_answer)
 
     return digit_sum
 
-    # start = math.sqrt(2)
-    # a0 = math.floor(math.sqrt(2))
 
-    # r = start - a0
-    # print(r)
-    # print(math.floor(1 / r))
-    # a1 = math.floor(1 / r)
-
-    # convergent = a0 + 1 / math.floor(1 / r)
-    # print("convergent is ", convergent)
-
-    # new_r = simplify((1 / r) - a1)
-    # a2 = math.floor(1 / new_r)
-
-    # convergent = a0 + 1 / (a1 + 1 / math.floor(1 / new_r))
-    # print("convergent is ", convergent)
-
-    # very_new_r = simplify((1 / new_r) - a2)
-    # a3 = math.floor(1 / very_new_r)
-
-    # convergent = a0 + 1 / (a1 + 1 / (a2 + 1 / math.floor(1 / very_new_r)))
-    # print("convergent is ", convergent)
-
-
-
-
-
-
-
-    
-
-
-print(find_convergents(100))
+print("Answer to problem 65: ", find_convergents(100))
