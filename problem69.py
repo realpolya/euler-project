cdfc851089ab79761f4d7aecb0b4bf648f367efa
realpolya <ps2780@columbia.edu>
2025-05-28 02:35:23 -0400

    polish problem69 assignment

--- problem69.py
+++ problem69.py
@@ -1,3 +1,13 @@
 '''
+Totient Maximum
+
+Euler's totient function, phi(n) [sometimes called the phi function], 
+is defined as the number of positive integers not exceeding n which 
+are relatively prime to n. For example, as 1, 2, 4, 5, 7, and 8, 
+are all less than or equal to nine and relatively prime to nine, 
+phi(9)=6.
+
+It can be seen that n = 6 produces a maximum n/phi(n) for n <= 10.
+Find the value of n <= 1,000,000 for which n/phi(n) is a maximum.
 
 '''
