ec733e393d602a2a5f2afd923e65bccf3ed6c807
realpolya <ps2780@columbia.edu>
2025-06-02 12:54:34 +0300

    problem 69: working on coprime function

--- problem69.py
+++ problem69.py
@@ -11,3 +11,56 @@ It can be seen that n = 6 produces a maximum n/phi(n) for n <= 10.
 Find the value of n <= 1,000,000 for which n/phi(n) is a maximum.
 
 '''
+
+# find a number of positive integers below n
+# that are relatively prime to n
+
+def is_relatively_prime(num1, num2):
+
+    # divisors1 = set()
+    # divisors2 = set()
+
+    max_num = 0
+
+    if num1 > num2:
+        max_num = num1
+    elif num2 > num1 or num1 == num2:
+        max_num = num2
+
+    # must not have common divisors greater than 1
+
+    # calculate divisors for each n
+    for n in range(2, max_num):
+
+        divisor_true = [False, False]
+
+        if num1 % n == 0:
+            # divisors1.add(n)
+            divisor_true[0] = True
+        
+        if num2 % n == 0:
+            # divisors2.add(n)
+            divisor_true[1] = True
+        
+        if all(divisor_true):
+            return False
+
+    # is there overlap?
+    return True
+
+
+def totient_max(limit=10):
+
+    max_toti = 0
+
+    for n in range(2, limit+1):
+
+        prime_count = 1 # number 1 is already included
+
+        for candidate in range(2, n):
+
+
+
+    return max_toti
+
+
