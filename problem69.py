350f7c8ae1b3442205c4fb00cf9d48a3b59612ab
realpolya <ps2780@columbia.edu>
2025-06-02 21:50:12 +0300

    solve problem 69, solution takes 18 seconds

--- problem69.py
+++ problem69.py
@@ -12,59 +12,101 @@ Find the value of n <= 1,000,000 for which n/phi(n) is a maximum.
 
 '''
 
+import math
+from extras.utils import prime_factorization, sieve_eratosthenes
+
 # find a number of positive integers below n
 # that are relatively prime to n
 
-def find_divisors(n):
+# def find_divisors(n):
 
-    divisors = set()
-    # divisors.add(1)
-    divisors.add(n)
+#     divisors = set()
+#     more_divisors = set()
+#     # divisors.add(1)
 
-    for num in range(2, n):
+#     for num in range(2, math.floor(math.sqrt(n))+1):
 
-        if n % num == 0:
-            divisors.add(num)
+#         if n % num == 0:
+#             divisors.add(num)
+    
+#     for num in divisors:
 
-    return divisors
+#         more_divisors.add(n / num)
+
+#     divisors.add(n)
+#     divisors.update(more_divisors)
+
+#     return divisors
 
 
 def totient_max(limit=10):
 
     max_toti = 0
     max_n = 0
+    mill_primes_unsorted = sieve_eratosthenes(1000000)
+    mill_primes = sorted(mill_primes_unsorted)
 
     for n in range(2, limit+1):
 
         # print("starting with n ", n)
-        prime_count = 1 # number 1 is already included
-        n_divisors = find_divisors(n)
+        # prime_count = 1 # number 1 is already included
+
+        # n_divisors = find_divisors(n)
         # print("divisors of n are ", n_divisors)
         # relative_primes = set()
         # relative_primes.add(1)
 
-        for candidate in range(2, n):
+        n_primes = []
 
-            coprime = True
+        for prime in mill_primes:
 
-            # if is_relatively_prime(n, candidate):
-            # print("for n ", n, "testing candidate", candidate)
+            # print("prime is ", prime, "n is ", n)
 
-            for num in range(2, candidate + 1):
+            if prime > math.sqrt(n):
+                break
 
-                # print("candidate is ", candidate, "divided by ", num, "remainder is ", candidate % num)
+            n_primes = prime_factorization(n, prime) + n_primes
 
-                if candidate % num == 0 and num in n_divisors:
-                    # print("candidate is out: ", candidate, "it is divisible by ", num, "which is in n_divisors", n_divisors)
-                    coprime = False
-                    break
+        n_prime_set = set(n_primes)
+        # n_primes = prime_factorization(n)
+
+        phi = n
+
+        for prime in n_prime_set:
+
+            # totient formula
+            phi *= (1 - (1/prime))
+
+
+        # for candidate in range(2, n):
+
+
+
+
+        #     if candidate % 2 == 0 and 2 in n_divisors:
+        #         continue
+
+        #     coprime = True
+
+        #     # if is_relatively_prime(n, candidate):
+        #     # print("for n ", n, "testing candidate", candidate)
+
+        #     for num in range(2, candidate + 1):
+
+        #         # print("candidate is ", candidate, "divided by ", num, "remainder is ", candidate % num)
+
+        #         if candidate % num == 0 and num in n_divisors:
+        #             # print("candidate is out: ", candidate, "it is divisible by ", num, "which is in n_divisors", n_divisors)
+        #             coprime = False
+        #             break
         
-            if coprime:
-                # print("coprime found")
-                prime_count += 1
-                # relative_primes.add(candidate)
+        #     if coprime:
+        #         # print("coprime found")
+        #         prime_count += 1
+        #         # relative_primes.add(candidate)
             
-        ratio = n / prime_count
+        # ratio = n / prime_count
+        ratio = n / phi
 
         # print("for n ", n, "coprimes are ", relative_primes)
 
