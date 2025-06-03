bda8e92a2aa71886c190cfcf015c2d9fe383cc34
realpolya <ps2780@columbia.edu>
2025-06-03 19:30:15 +0300

    introduce a new prime factors function

--- problem70.py
+++ problem70.py
@@ -24,11 +24,40 @@ from extras.utils import prime_factorization, sieve_eratosthenes
 def is_permutation(num1, num2):
     '''compares whether two numbers are permutations of each other'''
 
+    if num1 == num2:
+        return False
+
     # see if lists are identical
     if sorted(list(str(num1))) == sorted(list(str(num2))):
         return True
     
     return False
+    
+def get_prime_factors(n, primes):
+    '''non-recursive approach'''
+
+    prime_factors = set()
+    new_n = n
+
+    for prime in primes:
+
+        if prime > math.sqrt(n):
+            break
+        
+        if new_n % prime == 0:
+
+            prime_factors.add(prime)
+
+            while new_n % prime == 0:
+                new_n /= prime
+    
+    if new_n > 1 and new_n.is_integer():
+        prime_factors.add(int(new_n))
+    
+    return prime_factors
+
+
+# print(get_prime_factors(54, sorted(sieve_eratosthenes(1000000))))
 
 
 def toti_perm(limit=100000):
@@ -40,29 +69,41 @@ def toti_perm(limit=100000):
 
     for n in range(2, limit+1):
 
-        n_primes = []
+        # prime factors of n
+        n_primes = get_prime_factors(n, mill_primes)
 
-        for prime in mill_primes:
+        # for prime in mill_primes:
 
-            if prime > math.sqrt(n):
-                break
+        #     if prime > math.sqrt(n):
+        #         break
 
-            n_primes = prime_factorization(n, prime) + n_primes
+        #     n_primes = prime_factorization(n, prime) + n_primes
 
-        n_prime_set = set(n_primes)
+        # n_prime_set = set(n_primes)
+
+        # find co-factors
 
         phi = n
 
-        for prime in n_prime_set:
+        for prime in n_primes:
 
             # totient formula
             phi *= (1 - (1/prime))
+        
+        if phi.is_integer():
+            phi = int(phi)
+        else:
+            continue
+        
+        if n == 87109:
+            print("phi is ", phi)
+            print("primes are", n_primes)
 
         # check if permutation
         if not is_permutation(n, phi):
             continue
 
-        print("permnutation found, n is ", n, "phi is", phi)
+        print("permutation found, n is ", n, "phi is", phi)
 
         # calculate ratio
         ratio = n / phi
