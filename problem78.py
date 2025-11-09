f79ec1093cd39961403b0ec997a1971c09443639
realpolya <ps2780@columbia.edu>
2025-11-08 19:00:10 -0500

    copy function from problem 76

--- problem78.py
+++ problem78.py
@@ -27,7 +27,28 @@ def coin_partitions(coins=COINS):
 
     # divisible by 1 million
 
+
+# copy def from problem76
+def dynamic_programming(limit=LIMIT):
+    ''' dynamic programming approach to computing partitions '''
+
+    # dynamic programming list
+    dp_list = [0] * (limit + 1)
+    dp_list[0] = 1
+
+    print(dp_list)
+
+    for k in range(1, limit + 1):
+
+        for j in range(k, limit + 1):
+            # nested loops with indices
+
+            dp_list[j] += dp_list[j-k]
+            print("dp list now is ", dp_list)
     
+    print(dp_list)
+
+    return (dp_list[limit] - 1)
 
 
 
