6550b4b2b4dec6fefd2ae7e0a3d70356c32315e6
realpolya <ps2780@columbia.edu>
2025-12-08 15:01:50 -0500

    work on problem 79

--- problem79.py
+++ problem79.py
@@ -16,40 +16,111 @@ possible secret passcode of unknown length.
 '''
 
 from extras.problem79_extra import ATTEMPTS
+import math
+from itertools import combinations
 
-def derive_passcode():
-    # the numbers are always in order
 
-    # initiate a passcode variable
-    passcode = ''
+# compare every two attempts, then merge and compare every four (up until you have all 50)
 
-    count = 0 # temp count
+def compare_lists(list1, list2):
+    ''' compare two lists of numbers and return a combination with possible order '''
 
-    # loop through attempts
-    for attempt in ATTEMPTS:
+    new_order = list1.copy()
 
-        digits = str(attempt)
+    for digit in list2:
 
-        # loop through digits
-        for digit in digits:
-
-            # if digits are not seen before, concatenate
-            if str(digit) not in passcode:
-
-                passcode += str(digit)
+        if digit not in new_order:
+            new_order.append(digit)
         
-        count += 1
-
-        if count > 2:
-            break
-
-    # follow the order of digits until something does not click
-
-    print(passcode)
+    # 680 and 180 can return two possibilities: 6180 and 1680
 
 
+# branching would become too extreme (tree too large)
 
 
-# print(ATTEMPTS)
+def derive_brute_force():
 
-derive_passcode()
+    # get all of the possible digits
+    possible_digits = set()
+
+    for attempt in ATTEMPTS:
+        digits = list(str(attempt))
+
+        for digit in digits:
+            possible_digits.add(digit)
+    
+    print(possible_digits)
+
+    print(len(ATTEMPTS))
+    print(math.factorial(8))
+    print(math.factorial(8) * len(ATTEMPTS))
+
+    factorial_obj = {}
+
+    factorial_digits = list(possible_digits)
+
+    possible_combos = combinations(factorial_digits, 8) # FIXME: get all possible combos with all digits
+    # print(possible_combos)
+
+    for combo in possible_combos:
+        print("hello", combo)
+
+
+
+derive_brute_force()
+
+
+
+
+
+# def derive_passcode():
+#     # the numbers are always in order
+
+#     # initiate a passcode variable
+#     passcode = ''
+#     passcode_arr = []
+
+#     count = 0 # temp count
+
+#     # loop through attempts
+#     for attempt in ATTEMPTS:
+
+#         digits = list(str(attempt))
+        
+#         # loop through digits
+#         for i, digit in enumerate(digits):
+
+#             # if digit is not seen before, concatenate
+#             if str(digit) not in passcode:
+
+#                 passcode += str(digit)
+#                 passcode_arr.append(digit)
+
+#             # if digit is seen before
+#             elif str(digit) in passcode:
+
+#                 # compare indices in passcode array and in digits list
+
+
+#                 # loop through passcode digits
+
+#                 # compare order of the digits
+
+#                 # must not contradict previous attempts
+        
+#         count += 1
+
+#         if count > 2:
+#             break
+
+#     # follow the order of digits until something does not click
+
+#     print(passcode)
+#     print(passcode_arr)
+
+
+
+
+# # print(ATTEMPTS)
+
+# derive_passcode()
