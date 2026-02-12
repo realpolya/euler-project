1b5c66b748410e455bdf391017d82cae8b6abe00
realpolya <ps2780@columbia.edu>
2026-02-12 13:36:39 -0500

    update count decimal digits

--- problem80.py
+++ problem80.py
@@ -19,7 +19,7 @@ from decimal import Decimal, getcontext, localcontext
 def count_decimal_digits(num=2):
 
     # length of digits
-    target_len = 100
+    target_len = 99
     guard = 25
     work_precision = target_len + guard
 
@@ -44,24 +44,26 @@ def count_decimal_digits(num=2):
         
         # force fixed point output to see all of the digits
         fixed_point = format(x, 'f')
-        frac_part = fixed_point.split('.', 1)[1]
+        int_part, frac_part = fixed_point.split('.', 1)
         zeros = "0" * target_len
         frac_part = (frac_part + zeros)[:target_len]
 
         frac_sum = sum(int(d) for d in frac_part)
+        frac_sum += int(int_part)
+
         return frac_sum
     
 
-def sum_decimal_digits_sqrt(num=2):
+# def sum_decimal_digits_sqrt(num=2):
 
-    target_len = 100
-    scaled = math.isqrt(num * 10 ** (2 * target_len))
+#     target_len = 100
+#     scaled = math.isqrt(num * 10 ** (2 * target_len))
 
-    s = str(scaled).zfill(target_len + 1)
-    frac = s[1:]
-    frac_sum = sum(int(char) for char in frac)
-    return frac_sum
+#     s = str(scaled).zfill(target_len + 1)
+#     frac = s[1:]
+#     frac_sum = sum(int(char) for char in frac)
+#     return frac_sum
 
 
 print("for number 2, the count is: ", count_decimal_digits())
-print("for number 2, the count of second function is: ", sum_decimal_digits_sqrt())
+# print("for number 2, the count of second function is: ", sum_decimal_digits_sqrt())
